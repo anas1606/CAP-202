6547a5479818d2eedfc43fed5953a8741d2989af
Loriya Anas <48900508+anas1606@users.noreply.github.com>
2020-01-20 20:26:54 +0530

    Add files via upload

---
+++ IF ELSE/grt_two_value.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+	int no1,no2;
+	
+	
+	  cout<<"Enter the TWO Number: ";
+	  cin>>no1>>no2;
+	  
+      	
+	   if (no1 == no2)
+	   {
+	   	cout<<"Both Are EquaL"<<endl;
+	   }
+	   if(no1 > no2)
+	   {
+	   	cout<<"No1 is GreateR"<<endl;
+	   }
+	   else
+	   {
+	   	cout<<"No2 is GreateR"<<endl;
+	   }
+	
+}
