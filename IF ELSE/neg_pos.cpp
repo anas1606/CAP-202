d340aff12942660a8bc7f6afb2ed77b42792a39e
Loriya Anas <48900508+anas1606@users.noreply.github.com>
2020-01-20 20:46:07 +0530

    Add files via upload

---
+++ IF ELSE/neg_pos.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+	int no;
+	
+		cout<<"Enter The Number: ";
+		cin>>no;
+		
+		 	if(no==0)
+		 	{
+		 	  cout<<"Given No is ZerO";	
+			}
+			else if(no > 0)
+			{
+				cout<<"Given No is Positive";
+			}
+			else
+			{
+				cout<<"Given No is Negative";
+			}
+}
