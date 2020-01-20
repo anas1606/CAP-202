2451d9ce7d45e2895dbc0cae6eaff19e0143ff1c
Loriya Anas <48900508+anas1606@users.noreply.github.com>
2020-01-20 20:23:22 +0530

    Add files via upload

---
+++ IF ELSE/odd_even.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+	int no;
+	
+	
+	  cout<<"Enter the NO: ";
+	  cin>>no;
+	  
+      	
+	   if (no == 0)
+	   {
+	   	cout<<"NO is ZERO"<<endl;
+	   }
+	   if(no % 2 == 0)
+	   {
+	   	cout<<"No is EVEN"<<endl;
+	   }
+	   else
+	   {
+	   	cout<<"No is ODD"<<endl;
+	   }
+	
+}
